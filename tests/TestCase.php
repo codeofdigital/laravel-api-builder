0bf07b22408252bfc4a496534da953d1a27c66c4
bryanadamloh97 <bryanloh@talentcloud.ai>
2021-07-28 10:43:49 +0800

    Update composer and moved factories folder to root

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -38,7 +38,7 @@ abstract class TestCase extends OrchestraTestCase
     {
         parent::setUp();
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->withFactories(__DIR__.'/database/factories');
+        $this->withFactories(__DIR__.'/../database/factories');
     }
 
     /**
